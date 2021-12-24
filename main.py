b3343b6da6eeac57fde645cb4f09dcc8995b4231
Neo <78875228+NeoPythonProjects@users.noreply.github.com>
2021-12-24 11:56:16 +0000

    started with Kivy

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager
+
 from encryption import get_encryption_key, encrypt, decrypt
 from decorators import logged_in
 from login import encrypt_password
+
+Builder.load_file('gui.kv')
+
+class LoginScreen(Screen):
+  pass
+
+class RootWidget(ScreenManager):
+  pass
+
+class MainApp(App):
+  def builder(self):
+    return RootWidget()
+  
+
 
 @logged_in
 def main(user: str, pw: str) -> None:
@@ -10,6 +28,7 @@ def main(user: str, pw: str) -> None:
 
 if __name__ == "__main__":
   pass
+  MainApp().run()
   # msg_utf = "testing 1 2 3 in the place to be"
   # key = get_encryption_key()
   # msg_enc_b = encrypt(msg_utf, key)
